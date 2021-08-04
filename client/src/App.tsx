6b05c84ee8961f777be27d72c2278d6b315e07de
homoludens49 <homoludens49@gmail.com>
2021-08-04 15:16:28 +0300

    updates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,11 @@ import React, { FC, ChangeEvent, useState, useEffect } from "react";
 import "./App.css";
 import TodoTask from "./Components/TodoTask";
 import { ITask } from "./interfaces";
-import { IMessage } from "./messageInterface";
+
 import { io } from "socket.io-client";
 import axios from "axios";
 
+//creat socket connection
 let socket: any;
 // const URL = "https://todoassignmentpjotrssakovs.herokuapp.com";
 const URL = "http://localhost:1337";
@@ -15,44 +16,36 @@ let connectionOptions: any = {
 };
 
 const App: FC = () => {
+  //create socket connection params
   const [loggedIn, setLoggedIn] = useState<boolean>(false);
   const [name, setName] = useState<string>("");
-  const [room, setRoom] = useState<number>(0);
-
+  const [room] = useState<number>(0);
+  //initiate socket connection
   useEffect(() => {
     socket = io(URL, connectionOptions);
   }, [URL]);
-
+  //join room on enter name
   const logIn = (): void => {
     setLoggedIn(!loggedIn);
     socket.emit("join_room", room);
   };
-
-  //
-  const [message, setMessage] = useState<string>("");
-  const [messageList, setMessageList] = useState<IMessage[]>([]);
-
-  useEffect(() => {
-    socket.on("receive_message", (data: IMessage) => {
-      setMessageList([...messageList, data]);
-    });
-  });
 
   //Todo
   const [task, setTask] = useState<string>("");
   const [deadline, setDeadline] = useState<number>(0);
   const [hide, setHide] = useState<boolean>(false);
   const [todoList, setTodoList] = useState<ITask[]>([]);
-
+  //get Todos from DB on init
   useEffect(() => {
     getTasks();
   }, []);
+  //get Todos from DB on init
   const getTasks = async () => {
     //const res = await axios.get("https://todoassignmentpjotrssakovs.herokuapp.com/tasks");
     const res = await axios.get("http://localhost:1337/tasks");
     setTodoList(res.data);
   };
-
+  //sockets to display changes in Todos
   useEffect(() => {
     socket.on("display_todo", (data: ITask) => {
       setTodoList([...todoList, data]);
@@ -64,6 +57,7 @@ const App: FC = () => {
     });
   });
 
+  //handle change of input fields
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     event.target.name === "task"
       ? setTask(event.target.value)
@@ -72,20 +66,9 @@ const App: FC = () => {
       : setDeadline(Number(event.target.value));
   };
 
-  const sendMessage = async () => {
-    const messageContent = {
-      room: room,
-      content: {
-        name: name,
-        message: message,
-      },
-    };
-    await socket.emit("send_message", messageContent);
-    setMessageList([...messageList, messageContent.content]);
-    setMessage("");
-  };
-
+  //add Todo
   const addTask = async () => {
+    //create new ITask
     const newTask = {
       user: name,
       taskName: task,
@@ -95,7 +78,9 @@ const App: FC = () => {
     };
     setTodoList([...todoList, newTask]);
     //await axios.post("https://todoassignmentpjotrssakovs.herokuapp.com/tasks", newTask);
+    //add todo to DB
     await axios.post("http://localhost:1337/tasks", newTask);
+    //reflect todo for others through socket
     await socket.emit("create_todo", newTask, room);
     setTask("");
     setDeadline(0);
@@ -107,36 +92,43 @@ const App: FC = () => {
     // setTodoList(todoList.filter((task) => task.taskName != taskNameToEdit));
     //this is
   };
-
+  //Toogle complete task
   const completeTask = (task: ITask) => {
     const updatedArray = [...todoList];
     for (let i in updatedArray) {
       if (updatedArray[i].taskName === task.taskName) {
         updatedArray[i].complete = !updatedArray[i].complete;
-        // axios.put(
-        //   `https://todoassignmentpjotrssakovs.herokuapp.com/tasks/${updatedArray[i].taskName}`,
-        //   updatedArray[i]
-        // );
+        //update todo in DB
         axios.put(
           `http://localhost:1337/tasks/${updatedArray[i].taskName}`,
           updatedArray[i]
         );
+        //reflect todo deletion for others through socket
         socket.emit("update_todo", updatedArray, room);
         return setTodoList(updatedArray);
       }
     }
   };
-
+  //handle task deletion
   const deleteTask = (task: ITask) => {
     const updatedArray = todoList.filter((ts) => ts.taskName !== task.taskName);
     //axios.delete(`https://todoassignmentpjotrssakovs.herokuapp.com/tasks/${task.taskName}`);
+    //delete todo from DB
     axios.delete(`http://localhost:1337/tasks/${task.taskName}`);
+    //reflect todo deletion for others through socket
     socket.emit("update_todo", updatedArray, room);
     setTodoList(updatedArray);
   };
 
   return (
     <div className="App">
+      {!loggedIn ? (
+        <div>
+          <small className="intro">
+            Type your name heve to see what others are doing
+          </small>
+        </div>
+      ) : undefined}
       <div className="header">
         {!loggedIn ? (
           <div className="logIn">
@@ -163,37 +155,12 @@ const App: FC = () => {
                 type="number"
                 placeholder="Deadline (in Days)..."
                 name="deadline"
-                value={deadline}
                 onChange={handleChange}
               ></input>
             </div>
             <button onClick={addTask}>Add Task</button>
           </div>
         )}
-      </div>
-      <div className="chatContainer">
-        <div className="messages">
-          {messageList.map((val, key) => {
-            return (
-              <div
-                className="messageContainer"
-                id={val.name === name ? "You" : "Other"}
-              >
-                <div className="individualMessage">
-                  {val.name}: {val.message}
-                </div>
-              </div>
-            );
-          })}
-        </div>
-        <div className="messageInputs">
-          <input
-            type="text"
-            placeholder="Message..."
-            onChange={(e) => setMessage(e.target.value)}
-          ></input>
-          <button onClick={sendMessage}>Send</button>
-        </div>
       </div>
       <div className="todoList">
         <button onClick={() => setHide(!hide)}>Hide completed Tasks</button>
